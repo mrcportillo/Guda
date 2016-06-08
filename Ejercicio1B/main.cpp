e7e41439e4893a8aaa70ab3e986cade44c0ab5b8
root <root@mrc.mrc.dom>
2016-06-07 21:15:54 -0300

    agregando codigo

--- Ejercicio1B/main.cpp
+++ Ejercicio1B/main.cpp
@@ -1,8 +1,25 @@
-#include <QCoreApplication>
+#include "stdafx.h"
+#include "cv.h"
+#include "cvaux.h"
+#include "highgui.h"
+#include <stdio.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    char name0[] = "/home/mrc/ProyectosQt/Guda/Ejercicio1/Ejercicio1/img/1.jpg";
+    char name1[] = "/home/mrc/ProyectosQt/Guda/Ejercicio1/Ejercicio1/img/resized.jpg";
+    char name2[] = "/home/mrc/ProyectosQt/Guda/Ejercicio1/Ejercicio1/img/corte.jpg";";
+    float dx = 2;
+    float dy = 2;
+    int deltax = 50;
+    int deltay = 50;
+    int i = 0, j = 0, R = 0, G = 0, B = 0;
 
-    return a.exec();
+    CvScalar s;
+    CvRect faceCoords;
+    IplImage* imagen = NULL;
+    IplImage* faceImagen = NULL;
+    imagen = cvLoadImage(name0, 1);
+    cvNamedWindow("Fuente", 1);
+    cvShowImage("Fuente", imagen);
 }
