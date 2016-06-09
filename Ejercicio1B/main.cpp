127586cda8e5444eee021162e378cb8cdcc237fd
root <root@deb8-mrc.mrc-dom>
2016-06-09 20:11:17 +0000

    agregado codigo

--- Ejercicio1B/main.cpp
+++ Ejercicio1B/main.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+//#include "stdafx.h"
 #include "cv.h"
 #include "cvaux.h"
 #include "highgui.h"
@@ -7,8 +7,8 @@
 int main()
 {
     char name0[] = "/home/mrc/ProyectosQt/Guda/Ejercicio1/Ejercicio1/img/1.jpg";
-    char name1[] = "/home/mrc/ProyectosQt/Guda/Ejercicio1/Ejercicio1/img/resized.jpg";
-    char name2[] = "/home/mrc/ProyectosQt/Guda/Ejercicio1/Ejercicio1/img/corte.jpg";";
+    char name1[] = "/home/mrc/ProyectosQt/Guda/Ejercicio1/Ejercicio1/img/resizedB.jpg";
+    char name2[] = "/home/mrc/ProyectosQt/Guda/Ejercicio1/Ejercicio1/img/corteB.jpg";
     float dx = 2;
     float dy = 2;
     int deltax = 50;
@@ -22,4 +22,30 @@ int main()
     imagen = cvLoadImage(name0, 1);
     cvNamedWindow("Fuente", 1);
     cvShowImage("Fuente", imagen);
+
+    int py = imagen->height*dy;
+    int px = imagen->width*dx;
+    IplImage *resized = cvCreateImage(cvSize(px, py), IPL_DEPTH_8U, 3);
+    IplImage *imag = cvCreateImage(cvSize(imagen->height, imagen->width), IPL_DEPTH_8U, 3);
+
+    cvResize(imagen, resized, CV_INTER_CUBIC);
+    cvSaveImage(name1, resized);
+    cvNamedWindow("Resized", 1);
+    cvShowImage("Resized", resized);
+
+    faceCoords = cvRect(10, 10, 250, 300);
+    cvSetImageROI(resized, faceCoords);
+
+    for (i = 10; i <= 100; i++) {
+      for (j=10; j <= 150; j++) {
+        s = cvGet2D(resized, i, j);
+        R=s.val[0];
+        G=s.val[1];
+        B=s.val[2];
+        s.val[0]=100;
+        s.val[1]=120;
+        s.val[2]=200;
+      }
+    }
+    return 0;
 }
